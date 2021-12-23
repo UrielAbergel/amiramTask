02696e6e7b24f1bc1d71a56d22b11dac1b557b06
uabergel <uriel.abergel@cyberark.com>
2021-12-23 13:35:28 +0200

    add scroll options to the project improve ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react';
-import {getPostByPage} from './API/api';
-import Card from '@mui/material/Card';
-import RecipeReviewCard from './Components/Post';
-import { Container } from '@mui/material';
+
 import ListOfPost from './Components/ListOfPost';
 
 
 
 function App() {
 
-const [page, setPage] = useState(0);
-
   return (
     <div className="App">
-      <ListOfPost pageNumber={page}></ListOfPost>
+      <ListOfPost ></ListOfPost>
     </div>
   );
 }
--- src/Components/ListOfPost.js
+++ src/Components/ListOfPost.js
@@ -2,27 +2,37 @@ import * as React from 'react';
 import { Container, Grid } from '@mui/material';
 import Post from './Post';
 import { getPostByPage } from '../API/api';
+import { Waypoint } from 'react-waypoint';
 
 
-export default function ListOfPost(pageNumber) {    
+export default function ListOfPost() {    
 
     const [postList,setData] = React.useState([])
-    const [page,setPage] = React.useState(pageNumber)
+    const [page,setPage] = React.useState(0)
     React.useState(initializePostArray())
     
-    async function addArray(){
-    let postsObject = await getPostByPage(page,20);
-    let newPostArray = postList.push(...postsObject.data);
-    setData(newPostArray);
+    async function addArray(newPageNumber){
+    let postsObject = await getPostByPage(newPageNumber,20);
+    let newPosts = [...postList];
+    let tempPosts = [...postsObject.data];
+    let temp = [...newPosts, ...tempPosts];
+    setPage(newPageNumber)   
+    setData(temp)
+    
     }
 
-    async function initializePostArray(){
-    let postsObject = await getPostByPage(0,20);
+    async function initializePostArray(page){
+    let postsObject = await getPostByPage(page,20);
     let newPostArray = postsObject.data;
 
     if(postList.length === 0){
          setData(newPostArray);
         }
+    }
+
+    function handleChangePage(){
+        let currentPage = page;
+        setPage(currentPage + 1);
     }
 
 
@@ -31,11 +41,14 @@ export default function ListOfPost(pageNumber) {
         <Container>
             {console.log(postList)}
             <Grid container spacing={3}>
-                {postList.map((post) => (
-                    <Grid item key={post.id} xs={12} md={6} lg={4}>
-                        <Post post={post}/> 
-                    </Grid>
-                ))}
+                    {postList.map((post, index) => (
+                        <Grid item key={post.id} xs={12} md={6} lg={4}>
+                            <Post post={post}/> 
+                            { index === postList.length - 3 && (
+                                <Waypoint onEnter={() => addArray(page+1)}/> 
+                            )}
+                        </Grid>
+                    ))}                
             </Grid>
         </Container>
         </div>
